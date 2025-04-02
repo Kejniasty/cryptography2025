06275b9a6cdcbb1f37fe3fbe0ddee879e4d6b54a
251532 <Olciak5920048753@>
2025-04-02 19:24:58 +0200

    small size change

--- src/main/java/org/example/des/App.java
+++ src/main/java/org/example/des/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("App.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 440);
+        Scene scene = new Scene(fxmlLoader.load(), 650, 440);
         stage.setTitle("3DES");
         stage.setScene(scene);
         stage.show();
